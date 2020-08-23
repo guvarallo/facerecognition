646e36d6f3afa2700f777978b7dd0c9dd506d83c
guvarallo <gustavo.varallo@live.com>
2020-08-23 17:45:13 -0300

    Added form validation

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,8 +1,14 @@
 import React, { useState } from 'react';
+import { useForm } from "react-hook-form";
+import ErrorMessage from "./ErrorMessage";
+import "./Forms.css";
+
 
 function SignIn({ handleRouteChange, loadUser }) {
   const [email, setEmail] = useState('');
   const [pass, setPass] = useState('');
+  const { register, handleSubmit, errors } = useForm();
+
 
   function handleEmailChange(event) {
     setEmail(event.target.value);
@@ -12,8 +18,7 @@ function SignIn({ handleRouteChange, loadUser }) {
     setPass(event.target.value);
   }
   
-  function handleSubmitSignIn(event) {
-    event.preventDefault();
+  function handleSubmitSignIn() {
     fetch('https://thawing-caverns-91691.herokuapp.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
@@ -35,28 +40,49 @@ function SignIn({ handleRouteChange, loadUser }) {
   return (
     <div className='mt5'>
       <article className="ba dark-gray b--black-10 w-100 w-50-m w-25-l pa4 mv4 shadow-5 center br3">
-        <div className="">
-          <form className="w-100" >
-            <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-              <legend className="f2 fw6 ph0 mh0 tc">Sign In</legend>
-              <div className="mt3">
-                <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                <input onChange={handleEmailChange} className="pa2 input-reset ba bg-transparent hover-bg-purple hover-white w-100 br3" type="email" />
-              </div>
-              <div className="mv3">
-                <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                <input onChange={handlePassChange} className="b pa2 input-reset ba bg-transparent hover-bg-purple hover-white w-100 br3" type="password" />
-              </div>
-              <label className="pa0 ma0 lh-copy f6 pointer"><input type="checkbox" /> Remember me</label>
-            </fieldset>
-            <div className="">
-              <input onClick={handleSubmitSignIn} className="b ph3 pv2 f6 input-reset grow pointer bg-purple hover-bg-white hover-purple link ba dib white" type="submit" value="Sign in" />
+        <form className="App w-100" onSubmit={handleSubmit(handleSubmitSignIn)}>
+          <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+            <legend className="f2 fw6 ph0 mh0 tc">Sign In</legend>
+            <div className="mt3">
+              <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+              <input 
+                onChange={handleEmailChange} 
+                className="pa2 input-reset ba bg-transparent hover-bg-purple hover-white w-100 br3" 
+                type="email" 
+                name="email" 
+                ref={register({ required: true, pattern: /^\S+@\S+$/i })}
+              />
+              <ErrorMessage error={errors.email} />
             </div>
-            <div className="lh-copy mt3">
-              <a onClick={() => handleRouteChange('signup')} href="#0" className="f6 link dim black db b hover-white">Not registered yet? Sign up here!</a>
+            <div className="mv3">
+              <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+              <input 
+                onChange={handlePassChange} 
+                className="b pa2 input-reset ba bg-transparent hover-bg-purple hover-white w-100 br3" 
+                type="password" 
+                name="password" 
+                ref={register({ required: true })}
+              />
+              <ErrorMessage error={errors.password} />
             </div>
-          </form>
-        </div>
+          </fieldset>
+          <div className="">
+            <input 
+              className="b ph3 pv2 f6 input-reset grow pointer bg-purple hover-bg-white hover-purple link ba dib white" 
+              type="submit" 
+              value="Sign in"
+            />
+          </div>
+          <div className="lh-copy mt3">
+            <a 
+              onClick={() => handleRouteChange('signup')} 
+              href="#0" 
+              className="f6 link dim black db b hover-white"
+            >
+              Not registered yet? Sign up here!
+            </a>
+          </div>
+        </form>
       </article>
     </div>
   );
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
+import { useForm } from "react-hook-form";
+import ErrorMessage from "./ErrorMessage";
+import "./Forms.css";
+
 
 function SignUp({ handleRouteChange, loadUser, loadGuest }) {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [pass, setPass] = useState('');
+  const { register, handleSubmit, errors } = useForm();
 
   function handleNameChange(event) {
     setName(event.target.value);
@@ -17,8 +22,7 @@ function SignUp({ handleRouteChange, loadUser, loadGuest }) {
     setPass(event.target.value);
   }
 
-  function handleSubmitSignUp(event) {
-    event.preventDefault();
+  function handleSubmitSignUp() {
     fetch('https://thawing-caverns-91691.herokuapp.com/signup', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
@@ -42,28 +46,63 @@ function SignUp({ handleRouteChange, loadUser, loadGuest }) {
     <div className='mt5'>
       <article className="ba dark-gray b--black-10 w-100 w-50-m w-25-l pa4 mv4 shadow-5 center">
         <div className="">
-          <form className="w-100" >
+          <form className="App w-100" onSubmit={handleSubmit(handleSubmitSignUp)}>
             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
               <legend className="f2 fw6 ph0 mh0 tc">Sign Up</legend>
               <div className="mt3">
                 <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
-                <input className="pa2 input-reset ba bg-transparent hover-bg-purple hover-white w-100" type="text" onChange={handleNameChange} />
+                <input 
+                  className="pa2 input-reset ba bg-transparent hover-bg-purple hover-white w-100" 
+                  name="name" type="text" 
+                  onChange={handleNameChange} 
+                  ref={register({ required: true })}
+                />
+                <ErrorMessage error={errors.name} />
               </div>
               <div className="mt3">
                 <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                <input className="pa2 input-reset ba bg-transparent hover-bg-purple hover-white w-100" type="email" onChange={handleEmailChange} />
+                <input 
+                  className="pa2 input-reset ba bg-transparent hover-bg-purple hover-white w-100" 
+                  name="email" type="email" 
+                  onChange={handleEmailChange} 
+                  ref={register({ required: true, pattern: /^\S+@\S+$/i })}
+                />
+                <ErrorMessage error={errors.email} />
               </div>
               <div className="mv3">
                 <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                <input className="b pa2 input-reset ba bg-transparent hover-bg-purple hover-white w-100" type="password" onChange={handlePassChange} />
+                <input 
+                  className="b pa2 input-reset ba bg-transparent hover-bg-purple hover-white w-100" 
+                  name="password" 
+                  type="password" 
+                  onChange={handlePassChange} 
+                  ref={register({ required: true })}
+                />
+                <ErrorMessage error={errors.password} />
               </div>
             </fieldset>
             <div className="">
-              <input onClick={handleSubmitSignUp} className="b ph3 pv2 f6 input-reset grow pointer bg-purple hover-bg-white hover-purple link ba dib white" type="submit" value="Sign up" />
+              <input 
+                className="b ph3 pv2 f6 input-reset grow pointer bg-purple hover-bg-white hover-purple link ba dib white" 
+                type="submit" 
+                value="Sign up" 
+              />
             </div>
             <div className="lh-copy mt3">
-              <a onClick={() => handleRouteChange('signin')} href="#0" className="f6 link dim black db b hover-white">Already registered? Sign in here!</a>
-              <a onClick={() => loadGuest()} href="#0" className="f6 link dim black db b hover-white">Or just enter as a guest over here!</a>
+              <a 
+                onClick={() => handleRouteChange('signin')} 
+                href="#0" 
+                className="f6 link dim black db b hover-white"
+              >
+                Already registered? Sign in here!
+              </a>
+              <a 
+                onClick={() => loadGuest()} 
+                href="#0" 
+                className="f6 link dim black db b hover-white"
+              >
+                Or just enter as a guest over here!
+              </a>
             </div>
           </form>
         </div>
